ebe260ccadb6183f476ef03a78037a624ddb31f5
Roderick Sang <rodericksang@gmail.com>
2016-08-19 20:09:02 -0400

    Create Chapter-4-Exercises.py

---
+++ Chapter-4-Exercises.py
@@ -0,0 +1,35 @@
+# Exercise 4-3, Use a for loop to print the numbers from 1to 20, inclusive.
+# for numbers in range(1, 21):
+#     print(numbers)
+# -----
+# Exercise 4-4 Make a list of numbers from 1 to 1 million and use a for loop to print the numbers.
+# million = list(range(1, 1000001))
+# for number in million:
+#     print(number)
+# -----
+# Exercise 4-5 Make a list of the numbers from one to one millon
+# Use min() and max() to make sure your list actually starts at 1 and ends at a million.
+# Use sum() to see how quickly Python can add a million numbers.
+# million = list(range(1, 1000001))
+# print(min(million))
+# print(max(million))
+# print(sum(million))
+# -----
+# Exercise 4-6 Use the third argument of range() to make a list of odd numbers from 1 to 20.
+# Use a for loop to print each number.
+# odd = list(range(1, 21, 2))
+# for number in odd:
+#     print(number)
+# -----
+# Exercise 4-7 Make a list of the multiples of three from 3 to 30.
+# Use a for loop to print the numbers in your list.
+# threes = list(range(3, 30, 3))
+# for number in threes:
+#     print(number)
+# -----
+# Exercise 4-8 Make a list from 1 to 10.
+# Use a for loop to print the cube of each number
+# Bonus points if you use a list comprehension.
+# cubes = [number**3 for number in range(1, 11)]
+# print(cubes)
+# -----
